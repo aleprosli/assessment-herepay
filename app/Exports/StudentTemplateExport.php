a4cb6f6819c883430445a2f2d5c980d81d4b56c3
aliff <aliff.rosli96@gmail.com>
2023-07-22 19:12:23 +0800

    fix: headings add 1 example data

--- app/Exports/StudentTemplateExport.php
+++ app/Exports/StudentTemplateExport.php
@@ -2,18 +2,28 @@
 
 namespace App\Exports;
 
+use App\Models\User;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Faker\Factory as Faker;
 
 class StudentTemplateExport implements WithHeadings
 {
     public function headings(): array
     {
         return [
-            'Name',
-            'Level',
-            'Class',
-            'Parent Contact',
-        ];
+            [
+                'Name',
+                'Level',
+                'Class',
+                'Parent Contact',
+            ],
+            [
+                'Aliff',
+                '1',
+                'Delima',
+                '0135162634',
+            ]
+         ];
     }
 }
